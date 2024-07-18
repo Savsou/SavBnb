f67aec3273a472850bd98ea1d89accf9c28cb715
savonnas <savonna.sou@gmail.com>
2024-07-18 16:54:08 -0400

    add options in add-first-and-last-name migrations

--- backend/db/migrations/20240718043930-add-first-and-last-name-to-users.js
+++ backend/db/migrations/20240718043930-add-first-and-last-name-to-users.js
@@ -1,5 +1,10 @@
 'use strict';
 
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
@@ -12,12 +17,12 @@ module.exports = {
     await queryInterface.addColumn('Users', 'firstName', {
       type: Sequelize.STRING,
       allowNull: false,
-    });
+    }, options);
 
     await queryInterface.addColumn('Users', 'lastName', {
       type: Sequelize.STRING,
       allowNull: false,
-    });
+    }, options);
 
   },
 
@@ -28,7 +33,7 @@ module.exports = {
      * Example:
      * await queryInterface.dropTable('users');
      */
-    await queryInterface.removeColumn('Users', 'firstName');
-    await queryInterface.removeColumn('Users', 'lastName');
+    await queryInterface.removeColumn('Users', 'firstName', options);
+    await queryInterface.removeColumn('Users', 'lastName', options);
   }
 };
