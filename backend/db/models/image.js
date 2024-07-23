6d6927eda3387dee41785265b32b75fa28738be5
savonnas <savonna.sou@gmail.com>
2024-07-22 23:42:49 -0400

    add/modify tables User, Spot, Review, Booking, and Image

---
+++ backend/db/models/image.js
@@ -0,0 +1,40 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Image extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Image.belongsTo(models.Spot, {
+        foreignKey: 'imageableId',
+        constraints: false,
+        scope: {
+          imageableType: 'Spot'
+        }
+      });
+      Image.belongsTo(models.Review, {
+        foreignKey: 'imageableId',
+        constraints: false,
+        scope: {
+          imageableType: 'Review'
+        }
+      });
+    }
+  }
+  Image.init({
+    imageableId: DataTypes.INTEGER,
+    imageableType: DataTypes.STRING,
+    url: DataTypes.STRING,
+    preview: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'Image',
+  });
+  return Image;
+};
