952ae0fb00af91aff9468550170037c07a617e77
savonnas <savonna.sou@gmail.com>
2024-07-28 14:42:18 -0400

    create reviews route

---
+++ backend/routes/api/reviews.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const { Spot, Review, Image, User } = require('../../db/models');
+const { requireAuth } = require('../../utils/auth');
+const { handleValidationErrors } = require('../../utils/validation');
+const { check } = require('express-validator');
+
+const router = express.Router();
