68b54f30db60d9067f677240aac9b5c1729807fe
savonnas <savonna.sou@gmail.com>
2024-09-14 18:39:37 -0400

    css my login and signup buttons

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
@@ -5,10 +6,35 @@ import OpenModalButton from '../OpenModalButton';
 import LoginFormModal from "../LoginFormModal";
 import SignupFormModal from "../SignupFormModal/SignupFormModal";
 import { FaAirbnb } from "react-icons/fa";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { CgProfile } from "react-icons/cg";
 import './Navigation.css';
 
 const Navigation = ({ loading }) => {
     const sessionUser = useSelector((state) => state.session.user);
+    const [showMenu, setShowMenu] = useState(false);
+    const ulRef = useRef();
+
+    const toggleMenu = (e) => {
+        e.stopPropagation();
+        setShowMenu(!showMenu);
+    };
+
+    useEffect(() => {
+        if (!showMenu) return;
+
+        const closeMenu = (e) => {
+            if (ulRef.current && !ulRef.current.contains(e.target)) {
+                setShowMenu(false);
+            }
+        };
+
+        document.addEventListener('click', closeMenu);
+
+        return () => document.removeEventListener('click', closeMenu);
+    }, [showMenu]);
+
+    const ulClassName = "login-dropdown" + (showMenu ? "" : " hidden");
 
     const sessionLinks = sessionUser ? (
         <>
@@ -23,18 +49,36 @@ const Navigation = ({ loading }) => {
         </>
     ) : (
         <>
-            <li>
-                <OpenModalButton
-                    buttonText="Log In"
-                    modalComponent={<LoginFormModal />}
-                />
-            </li>
-            <li>
-                <OpenModalButton
-                    buttonText="Sign Up"
-                    modalComponent={<SignupFormModal />}
-                />
-            </li>
+            <button
+            style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                gap: "10px",
+                padding: "5px 10px",
+                color: "black",
+                fontSize: "30px",
+                borderRadius: "20px"
+            }}
+            onClick={toggleMenu}
+            >
+                <RxHamburgerMenu />
+                <CgProfile />
+            </button>
+            <ul className={ulClassName} ref={ulRef}>
+                <li>
+                    <OpenModalButton
+                        buttonText="Log In"
+                        modalComponent={<LoginFormModal />}
+                    />
+                </li>
+                <li>
+                    <OpenModalButton
+                        buttonText="Sign Up"
+                        modalComponent={<SignupFormModal />}
+                    />
+                </li>
+            </ul>
         </>
     );
 
